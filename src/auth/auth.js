256d39f1af27a1611d3480824c38ae4170cd3024
werivik <wervik00994@stud.noroff.no>
2025-04-28 20:43:56 +0200

    better organized

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,5 +1,5 @@
 import { headers } from "../headers";
-import { PROFILES_UPDATE, BOOKINGS_CREATE } from "../constants";
+import { PROFILES_UPDATE } from "../constants";
 
 export function getToken() {
   return localStorage.getItem('accessToken');
@@ -45,43 +45,5 @@ export const updateUserProfile = async (userId, { bio, avatar, banner, venueMana
   catch (error) {
     console.error('Error updating profile:', error);
     throw error;
-  }
-};
-
-export const handleBookingSubmit = async (checkInDate, checkOutDate) => {
-  try {
-    const token = localStorage.getItem('accessToken');
-
-    if (!token) {
-      console.warn('No access token found in localStorage');
-      alert('You need to log in first.');
-      return;
-    }
-
-    const response = await fetch(BOOKINGS_CREATE, {
-      method: 'POST',
-      headers: headers(token),
-      body: JSON.stringify({
-        dateFrom: checkInDate,
-        dateTo: checkOutDate,
-        guests,
-        venueId,
-      }),
-    });
-
-    if (!response.ok) {
-      const errorResponse = await response.text();
-      console.error("Booking submission failed with response:", errorResponse);
-      throw new Error(`Booking submission failed: ${response.status}`);
-    }
-
-    const data = await response.json();
-    console.log("Booking submitted successfully:", data);
-    alert('Booking successful!');
-  } 
-  
-  catch (error) {
-    console.error("Booking API error:", error);
-    alert('Booking submission failed. Please try again.');
   }
 };
