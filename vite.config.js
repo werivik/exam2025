502759a2feeacb582245c30fc335a8ea82c91255
werivik <wervik00994@stud.noroff.no>
2025-04-30 19:35:20 +0200

    trying to deploy to netlify

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root:'.',
   server: {
     port: 3000,
     open: true
   },
   build: {
+    outDir: 'dist',
     sourcemap: true
   }
 });
