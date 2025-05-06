061d201621cbccee383bb37fc0c342a051e1274d
werivik <wervik00994@stud.noroff.no>
2025-05-06 18:12:45 +0200

    added footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,6 +16,23 @@ function Footer() {
           <p>Elegance meets Comfort</p>
         </div>
         <div className={styles.footerRight}>
+          <div className={styles.FooterLinks}>
+            <Link><h3>Venues</h3></Link>
+            <Link><p>All Venues</p></Link>
+            <Link><p>Booked Venues</p></Link>
+            <Link><p>Favorite Venues</p></Link>
+          </div>
+          <div className={styles.FooterLinks}>
+            <Link><h3>Profile</h3></Link>
+            <Link><p>Costumer</p></Link>
+            <Link><p>Manager</p></Link>
+          </div>
+          <div className={styles.FooterLinks}>
+            <Link><h3>About Us</h3></Link>
+            <Link><p>About Us</p></Link>
+            <Link><p>Contact Us</p></Link>
+            <Link><p>Terms of Use</p></Link>
+          </div>
         </div>
       </div>
       <img src={edge} className={styles.footerEdgeRight}></img>
